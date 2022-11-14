195c042c6d8ee5d977db68baeeff92b4e51d0753
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-14 20:29:21 +0800

    add case 5

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -189,6 +189,27 @@ namespace double_linked_list
                                 obj.descending();
                             }
                             break;
+                        case '5':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break ;
+                                }
+                                node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record you want to search: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.noMhs);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
                     }
                 }
             }
