70e30e1c771e2bf473e84eef4ced1aa890bc3603
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-14 18:47:46 +0800

    create function if else in void descending

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -120,6 +120,22 @@ namespace double_linked_list
                     Console.Write(currentNode.noMhs + "" + currentNode.name + "\n");
             }
         }
+        public void descending()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is Empty");
+            else
+            {
+                Console.WriteLine("\nRecord in the Descending order of" + "roll number are:\n");
+                node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next) { }
 
+                while(currentNode != null)
+                {
+                    Console.WriteLine(currentNode.noMhs + "" + currentNode.name + "\n");
+                    currentNode = currentNode.prev;
+                }
+            }
+        }
     }
 }
